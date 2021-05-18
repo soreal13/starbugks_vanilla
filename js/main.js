29e4e759ffcfeff82dd3d6a62dc7b44021f46d2c
sojin13 <sodyniee@naver.com>
2021-05-18 17:02:55 +0900

    indentation 변경

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function() {
-    // 익명함수 핸들러 안에 로직
-    searchInputEl.focus(); 
+  // 익명함수 핸들러 안에 로직
+  searchInputEl.focus(); 
 });
 
 searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('foused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
+  searchEl.classList.add('foused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
 searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('foused');
-    searchInputEl.setAttribute('placeholder', '');
+  searchEl.classList.remove('foused');
+  searchInputEl.setAttribute('placeholder', '');
 });
